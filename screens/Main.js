3d4a5ace2a671bdc578362fd91b0e72a2a6fbaab
m-zaremba <zaremba_michal@wp.pl>
2019-05-12 12:28:47 +0200

    Help/info screen added

--- screens/Main.js
+++ screens/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import firebase from 'react-native-firebase';
 
-import {FlatList, ScrollView, View, Text, ImageBackground, StyleSheet} from 'react-native';
+import {FlatList, ScrollView, View, Text, ImageBackground, StyleSheet, Modal, TouchableOpacity} from 'react-native';
 import {createBottomTabNavigator, createStackNavigator, createAppContainer} from 'react-navigation';
 import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Ionicons';
+import ModalIcon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import AddSerie from './AddSerie';
 import Stats from './Stats';
@@ -20,7 +21,8 @@ class Main extends React.Component {
       .orderBy('timestamp', 'desc');
     this.state = {
       series: [],
-      loading: true
+      loading: true,
+      showModal: false
     };
   }
 
@@ -45,6 +47,18 @@ class Main extends React.Component {
       loading: false
     });
   };
+
+  showModal = () => {
+    this.setState({
+      showModal: true
+    })
+  }
+
+  hideModal = () => {
+    this.setState({
+      showModal: false
+    })
+  }
 
   componentDidMount() {
     this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
@@ -78,8 +92,42 @@ class Main extends React.Component {
               color='rgb(255, 57, 57)'
               onPress={() => this.props.navigation.navigate('AddSerie')}
             />
+            <Icon style={{position: 'absolute', top: 0, right: 5}} name='ios-help-circle' size={35} color='gray' onPress={() => {this.showModal()}}/>
           </View>
         </ImageBackground>
+        <Modal animationType='fade' transparent={true} visible={this.state.showModal} >
+          <TouchableOpacity activeOpacity={1} onPress={() => {this.hideModal()}} >
+            <View style={styles.modalView}>
+              <View style={styles.serieRow}>
+                <Text style={{fontSize: 22, color: 'black'}}>12.05.2019</Text>
+                <View style={{flexDirection: 'row'}}>
+                  <Icon name='ios-radio-button-off' size={30} style={styles.listIcon} />
+                  <Icon name='ios-radio-button-off' size={30} style={styles.listIcon} />
+                  <Icon name='ios-radio-button-off' size={30} style={styles.listIcon} />
+                  <Icon name='ios-radio-button-off' size={30} style={styles.listIcon} />
+                </View>
+              </View>
+              <View style={{flexDirection:'row', alignItems: 'flex-end'}}>
+                  <ModalIcon name='gesture-tap' size={40} color='rgb(255, 57, 57)'/>
+                  <Text style={styles.modalText}>Tap to view details</Text>
+              </View>
+
+                <View style={{...styles.serieRow, marginTop: 60}}>
+                  <Text style={{fontSize: 22, color: 'black'}}>12.05.2019</Text>
+                  <View style={{flexDirection: 'row'}}>
+                    <Icon name='ios-radio-button-off' size={30} style={styles.listIcon} />
+                    <Icon name='ios-radio-button-off' size={30} style={styles.listIcon} />
+                    <Icon name='ios-radio-button-off' size={30} style={styles.listIcon} />
+                    <Icon name='ios-radio-button-off' size={30} style={styles.listIcon} />
+                  </View>
+                </View>
+                <View style={{flexDirection:'row', alignItems: 'flex-end'}}>
+                <ModalIcon name='gesture-tap-hold' size={40} color='rgb(255, 57, 57)'/>
+                <Text style={styles.modalText}>Tap and hold to delete serie</Text>
+                </View>
+            </View>
+          </TouchableOpacity>
+        </Modal>
       </>
     );
   }
@@ -127,5 +175,39 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 40,
     backgroundColor: 'rgba(255, 255, 255, 0.4)'
-  }
+  },
+  modalView: {
+    backgroundColor: 'rgba(75, 75, 75, 0.95)',
+    paddingTop: 40,
+    paddingBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    height: '100%',
+    width: '100%',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  modalText: {
+    fontSize: 20,
+    color: 'white'
+  },
+  serieRow: {
+    height: 60,
+    marginTop: 3,
+    marginLeft: 5,
+    paddingLeft: 15,
+    paddingRight: 15,
+    marginRight: 5,
+    borderRadius: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.85)',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: '98%'
+  },
+  listIcon: {
+    color: 'black',
+    textAlign: 'center',
+    width: 30
+  },
 });
